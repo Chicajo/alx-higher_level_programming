9de4ef15a702f343c4904b5529072b7ae3cb3636
Chicajo <chiagoziesophia@gmail.com>
2022-10-24 22:27:01 +0100

    INHERITANCE

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# ikeji chiagozie @ alx-school
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
